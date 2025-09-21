85ddcf0266d7fee367849542ed7b3936700aa867
RustamKuramshin <leo.in.empty@gmail.com>
2025-09-22 00:21:29 +0300

    Initialize Spring Boot project with essential configurations and dependencies.

---
+++ src/main/java/ru/javaboys/vibe_data/VibeDataApplication.java
@@ -0,0 +1,13 @@
+package ru.javaboys.vibe_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/javaboys/vibe_data/VibeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaboys.vibe_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class VibeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
