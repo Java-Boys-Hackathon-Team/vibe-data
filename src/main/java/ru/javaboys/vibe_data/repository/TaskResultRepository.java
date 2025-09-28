1213ccf89aa6862282131858128f297f15196f7e
RustamKuramshin <leo.in.empty@gmail.com>
2025-09-28 04:40:42 +0300

    Переход на промежуточное сохранение результатов

--- src/main/java/ru/javaboys/vibe_data/repository/TaskResultRepository.java
+++ src/main/java/ru/javaboys/vibe_data/repository/TaskResultRepository.java
@@ -3,7 +3,9 @@ package ru.javaboys.vibe_data.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.javaboys.vibe_data.domain.TaskResult;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskResultRepository extends JpaRepository<TaskResult, UUID> {
+    Optional<TaskResult> findByTaskId(UUID taskId);
 }
