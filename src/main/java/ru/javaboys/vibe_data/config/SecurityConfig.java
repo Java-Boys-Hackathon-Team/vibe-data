d934213f2096e876d4ffd815838fc84d2fc7411b
Alex Ianchii <a.ianchii@mail.ru>
2025-10-01 12:05:22 +0300

    Добавлено разрешение на доступ к Swagger UI и API Docs.

--- src/main/java/ru/javaboys/vibe_data/config/SecurityConfig.java
+++ src/main/java/ru/javaboys/vibe_data/config/SecurityConfig.java
@@ -29,6 +29,10 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults())
