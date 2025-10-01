9545baa346232e9ce3043bdf9bb87a08a68966ab
Alex Ianchii <a.ianchii@mail.ru>
2025-10-01 11:55:42 +0300

    Добавлена интеграция Springdoc OpenAPI и базовая конфигурация API документов

---
+++ src/main/java/ru/javaboys/vibe_data/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package ru.javaboys.vibe_data.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiConfig {
+    private static final String schemeName = "basicAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Vibe Data API")
+                        .version("1.0.0"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local server"),
+                        new Server().url("https://api.vibe-data.javaboys.ru").description("Production server")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName,
+                                new SecurityScheme()
+                                        .name(schemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")));
+    }
+}
